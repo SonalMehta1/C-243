04b17fbb8f1523970319145211d2ea7eff2d8bd8
Sonal Mehta <63941997+SonalMehta1@users.noreply.github.com>
2023-09-01 10:22:32 +0530

    Add files via upload

---
+++ sketch1.ino
@@ -0,0 +1,15 @@
+int LED = 18;
+void setup() {
+
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+  delay(500);
+
+}
